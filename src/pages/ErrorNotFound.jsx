1af01930fbd4449500bc621cd0139f4c8696fbe6
dounya-m <mellouki.dounya@gmail.com>
2022-11-30 10:34:59 +0100

    update 404

--- src/pages/ErrorNotFound.jsx
+++ src/pages/ErrorNotFound.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function ErrorNotFound() {
   return (
     <div className='body_error '>
-        <section className='flex justify-end items-center gap-6 px-[6vw] pt-[3rem]'>
+        <section className='flex justify-end items-center gap-6 px-[6vw] pt-[6rem]'>
             <div className='flex flex-col justify-center'>
                 <h2 className='error_title'>Don't Cry</h2>
-                <button className='error_button'>Back to home</button>
+                <button className='error_button bg-blue-700 p-[0.5rem] rounded-xl hover:bg-blue-600'><Link to='/'>Back to home</Link> </button>
             </div>
             <div className='line_error'></div>
             <div>
                 <p className='error_404'>it's just error 404</p>
                 <p className='error_quotes'>The page your looking for is not availibale.</p>
             </div>
+        </section>
+        <section>
+            <h3 className='text-center text-[20rem] font-semibold'>404</h3>
         </section>
     </div>
   )
