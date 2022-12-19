463d856372777e33f920e488b8000efe96427355
dounya-m <mellouki.dounya@gmail.com>
2022-12-19 09:36:42 +0100

    3D car

--- src/components/three/Three.jsx
+++ src/components/three/Three.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Suspense } from 'react'
 import {Canvas} from 'react-three-fiber';
-import Scene from '../../Scene'
 
 function Three() {
 
   return (
     <Canvas>
-      <Suspense>
-        <Scene />
+      <Suspense fallback={null}>
+        {/* <Scene /> */}
       </Suspense>
     </Canvas>
   )
